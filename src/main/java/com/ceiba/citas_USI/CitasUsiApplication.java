8577abc53bb8fd334d19594aee990ccf97122efc
Iduar Albeiro Silva Muñoz - Ceiba Software <albeiro.silva@ceiba.com.co>
2021-07-30 11:11:49 -0500

    se agrega git al proyecto inicial

---
+++ src/main/java/com/ceiba/citas_USI/CitasUsiApplication.java
@@ -0,0 +1,13 @@
+package com.ceiba.citas_USI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitasUsiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitasUsiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ceiba/citas_USI/CitasUsiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceiba.citas_USI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitasUsiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
